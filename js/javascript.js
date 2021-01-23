75c39b2997841b6b6eed2c9cfabb0b2aab0d150b
Luis <luis@iMac-Salinas-Invernizzi.local>
2021-01-22 21:00:45 -0300

    dynami

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+
+let segundoAbout = document.querySelector('#segundoAbout')
+
+function leerMas() {
+    var nodoAbout = document.createElement("div")
+    nodoAbout.textContent("hola")
+    console.log(nodoAbout)
+}
